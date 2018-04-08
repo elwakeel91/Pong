ca04351c17f5b7490809980799e5cee1b28055c3
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-04-08 20:26:23 +0100

    Change the Ball behaviour
    Try to change the angle the ball bounces off the paddle depending on where it hits the paddle. Still needs tweaking.

--- Pong/Assets/Scripts/BallController.cs
+++ Pong/Assets/Scripts/BallController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
 
-    #region public variables
+    #region Public Variables
 
-    public float startSpeed = 1.0f;         // Start speed of the ball (default speed is 1)
+    public float startSpeed = 1.0f;         // Start speed of the ball (default is 1)
+    public float maxSpeed = 5.0f;           // Maximum speed of the ball (default is 5)
     public ParticleSystem trail;            // Particle system containing our ball trail
 
     public Transform playerOnePosition;     // The position of player one
@@ -15,7 +16,7 @@ public class BallController : MonoBehaviour
 
     #endregion
 
-    #region private variables
+    #region Private Variables
 
     // Ball properties
     float speed;                        // The current speed of the ball
@@ -33,7 +34,7 @@ public class BallController : MonoBehaviour
     #region Unity Methods
 
     /// <summary>
-    /// Initialise our variables
+    /// Method called when the object is enabled
     /// </summary>
     void Start()
     {
@@ -76,8 +77,23 @@ public class BallController : MonoBehaviour
         // Check if we collide with the players
         if (other.gameObject.CompareTag("Player"))
         {
+            Transform playerPosition = (transform.position.x > 0) ? playerTwoPosition : playerOnePosition;
+            // Check which part of the paddle we hit
+            float distanceFromCentre = (transform.position.y - playerPosition.position.y);
+            int direction = (distanceFromCentre > 0) ? 1 : -1;
+            // Calculate the angle we hit the paddle
+            float angle = transform.eulerAngles.x;
+            // Calculate the angle we get off the paddle (ranging from -60 to 60 degrees)
+            float newAngle = Mathf.Abs(angle) > Mathf.Abs(distanceFromCentre * 150.0f) ? angle * direction : (-distanceFromCentre * 150);
+            
+
             // Rotate the direction of the ball 180 degrees in the y axis
-            rb.MoveRotation(Quaternion.Euler(transform.eulerAngles.x, -transform.eulerAngles.y, transform.eulerAngles.z));
+            rb.MoveRotation(Quaternion.Euler(newAngle, -transform.eulerAngles.y, 0));
+            // Increase the speed of the ball by 0.25
+            speed += 0.25f;
+            // Make sure the speed doesn't exceed our max speed
+            speed = Mathf.Clamp(speed, 0.0f, maxSpeed);
+            Debug.Log(speed);
         }
 
         // Check if we collide with the walls
@@ -113,8 +129,10 @@ public class BallController : MonoBehaviour
     {
         // Start the trail particle system
         trail.Play();
-        // Rotate the ball 45 degrees in the x axis and 90 degrees in the y axis
-        rb.MoveRotation(Quaternion.Euler(45, 90, 0));
+        // Rotate the ball to 90 degrees in the y axis
+        rb.MoveRotation(Quaternion.Euler(0, 90, 0));
+        // Reset the speed of the ball
+        speed = startSpeed;
         // Start moving the ball
         isMoving = true;
     }
