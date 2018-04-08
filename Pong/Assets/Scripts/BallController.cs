9891384289d309903aebce82d6f299b058c7c293
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-04-08 18:59:54 +0100

    Add Pong 0.1

---
+++ Pong/Assets/Scripts/BallController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+
+    #region public variables
+
+    public float startSpeed = 1.0f;         // Start speed of the ball (default speed is 1)
+    public ParticleSystem trail;            // Particle system containing our ball trail
+
+    public Transform playerOnePosition;     // The position of player one
+    public Transform playerTwoPosition;     // The position of player two
+
+    #endregion
+
+    #region private variables
+
+    // Ball properties
+    float speed;                        // The current speed of the ball
+    Vector3 startPosition;              // The start position of the ball
+    bool isMoving;                      // Bool to check if the ball is allowed to move
+
+    // Unity properties
+    Rigidbody rb;                       // Rigid body component of the ball
+
+    // Other scripts
+    GameController gameController;      // Game Controller script
+
+    #endregion
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Initialise our variables
+    /// </summary>
+    void Start()
+    {
+        speed = startSpeed;                     // Initialise the speed
+        startPosition = transform.position;     // Initialise the start position
+        isMoving = false;                       // Initialise the movement vector
+
+        rb = GetComponent<Rigidbody>();         // Set the rigid body component
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();     // Set the game controller script
+    }
+
+    /// <summary>
+    /// Method called every frame
+    /// </summary>
+    void Update()
+    {
+        // Check if the user has pressed the space bar and the ball isn't moving
+        if (Input.GetKeyDown(KeyCode.Space) && !isMoving)
+            // Start moving the ball
+            StartMoving();
+    }
+
+    /// <summary>
+    /// Method called every physics frame
+    /// </summary>
+    void FixedUpdate()
+    {
+        // Check if we want to move the ball
+        if (isMoving)
+            // Move the ball
+            Move();
+    }
+
+    /// <summary>
+    /// Handles collisions
+    /// </summary>
+    /// <param name="other"></param>
+    void OnCollisionEnter(Collision other)
+    {
+        // Check if we collide with the players
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Rotate the direction of the ball 180 degrees in the y axis
+            rb.MoveRotation(Quaternion.Euler(transform.eulerAngles.x, -transform.eulerAngles.y, transform.eulerAngles.z));
+        }
+
+        // Check if we collide with the walls
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            // Rotate the direction of the ball 180 degrees in the x axis
+            rb.MoveRotation(Quaternion.Euler(-transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z));
+        }
+
+        // Check if we collide with the goal
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            // Reset the ball position
+            RestartPosition();
+
+            // Check if the ball is in the positive x axis
+            if (transform.position.x > 0)
+                // Player one scored
+                gameController.AddScore(1);
+            else // Player two scored
+                gameController.AddScore(2);
+        }
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Handles starting the movement of the ball
+    /// </summary>
+    public void StartMoving()
+    {
+        // Start the trail particle system
+        trail.Play();
+        // Rotate the ball 45 degrees in the x axis and 90 degrees in the y axis
+        rb.MoveRotation(Quaternion.Euler(45, 90, 0));
+        // Start moving the ball
+        isMoving = true;
+    }
+
+    /// <summary>
+    /// Handles restarting the position of the ball
+    /// </summary>
+    public void RestartPosition()
+    {
+        // Stop moving the ball
+        isMoving = false;
+        // Pause the trail particle system
+        trail.Stop();
+        // Return to the starting position
+        rb.position = startPosition;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Handles moving the ball towards the positive z axis
+    /// </summary>
+    void Move()
+    {
+        // Move the position of the rigid body using the positive z axis and speed
+        rb.MovePosition(transform.position + transform.forward * speed * Time.fixedDeltaTime);
+    }
+
+    #endregion
+
+}
---
+++ Pong/Assets/Scripts/GameController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    #region Public Variables
+
+    public float playerStartSpeed = 2;      // The start speed of the players
+    public Transform playerOnePosition;     // The position of player one
+    public Transform playerTwoPosition;     // The position of player two
+
+    public Text playerOneScoreText;         // The UI text for player one's score
+    public Text playerTwoScoreText;         // The UI text for player two's score
+
+    #endregion
+
+    #region Private Variables
+
+    float playerOneSpeed;       // Player one's current speed
+    float playerTwoSpeed;       // Player two's current speed
+    Vector3 movement;           // The movement vector for the player
+
+    int playerOneScore;         // Player one's score
+    int playerTwoScore;         // Player two's score
+
+    #endregion
+
+    #region Unity Methods
+
+    /// <summary>
+    /// Method called when the object is enables
+    /// </summary>
+    void Start()
+    {
+        // Initialise the speed of the players
+        playerOneSpeed = playerTwoSpeed = playerStartSpeed;
+
+        // Initialise the score
+        playerOneScore = 0;
+        playerTwoScore = 0;
+        UpdateScore();
+    }
+
+    /// <summary>
+    /// Method called every frame
+    /// </summary>
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Method called every physics frame
+    /// </summary>
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Vertical1") != 0)
+            MovePlayer(1);
+        if (Input.GetAxisRaw("Vertical2") != 0)
+            MovePlayer(2);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Adds one to the player's score
+    /// </summary>
+    /// <param name="playerNumber"></param>
+    public void AddScore(int playerNumber)
+    {
+        // Check which player has scored
+        switch(playerNumber)
+        {
+            case 1:
+                // Add 1 to player one's score
+                playerOneScore++;
+                break;
+            case 2:
+                // Add 1 to player two's score
+                playerTwoScore++;
+                break;
+        }
+        // Update the score
+        UpdateScore();
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Handles moving the player
+    /// </summary>
+    /// <param name="playerNumber"></param>
+    void MovePlayer(int playerNumber)
+    {
+        // Switch between the player number
+        switch (playerNumber)
+        {
+            case 1:
+                // Set the movement vector using player one's input
+                movement = new Vector3(0, playerOneSpeed, 0) * Input.GetAxisRaw("Vertical1");
+                // Move the player using the movement vector
+                playerOnePosition.Translate(movement * Time.deltaTime);
+                break;
+            case 2:
+                // Set the movement vector using player two's input
+                movement = new Vector3(0, playerTwoSpeed, 0) * Input.GetAxisRaw("Vertical2");
+                // Move the player using the movement vector
+                playerTwoPosition.Translate(movement * Time.deltaTime);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Handles updating the score
+    /// </summary>
+    void UpdateScore()
+    {
+        // Update player one's score
+        playerOneScoreText.text = playerOneScore.ToString();
+        // Update player two's score
+        playerTwoScoreText.text = playerTwoScore.ToString();
+    }
+
+    #endregion
+}
